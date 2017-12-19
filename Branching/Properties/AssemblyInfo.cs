9a19ea37f0eb62c46eff656d8ba346babb7b3db1
RupomAhsan <rupom@live.com>
2017-12-19 17:04:26 +0800

    Initialize Branching

--- Branching/Properties/AssemblyInfo.cs
+++ Branching/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImproveOOP")]
+[assembly: AssemblyTitle("Branching")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImproveOOP")]
+[assembly: AssemblyProduct("Branching")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("03b5fb11-9c28-478f-8b35-ca88ae6829ee")]
+[assembly: Guid("e6e65852-f085-47dc-b1a5-3ae8ff5d56b5")]
 
 // Version information for an assembly consists of the following four values:
 //
